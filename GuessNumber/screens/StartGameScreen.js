c5df00438866593b6055c9831e15f372b37629f6
Jaehong Ahn <100734717+jxxhong@users.noreply.github.com>
2025-01-31 20:33:07 +0900

    Feat : StartGameScreen

---
+++ GuessNumber/screens/StartGameScreen.js
@@ -0,0 +1,84 @@
+import { TextInput, View, StyleSheet, Alert } from 'react-native';
+import React, { useState } from 'react';
+import PrimaryButton from '../components/PrimaryButton';
+
+export default function StartGameScreen() {
+	const [enteredNumber, setEnteredNumber] = useState('');
+
+	function numberInputHandler(enteredText) {
+		setEnteredNumber(enteredText);
+	}
+
+	function resetInputHandler() {
+		setEnteredNumber('');
+	}
+
+	function confirmInputHandler() {
+		const choseNumber = parseInt(enteredNumber);
+
+		if (isNaN(choseNumber) || choseNumber <= 0 || choseNumber > 99) {
+			Alert.alert(
+				'유효하지 않은 숫자입니다.',
+				'숫자는 1과 99사이어야만 합니다.',
+				[{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }]
+			);
+			return;
+		}
+	}
+
+	return (
+		<View style={styles.inputContainer}>
+			<TextInput
+				style={styles.numberInput}
+				maxLength={2}
+				keyboardType="number-pad"
+				autoCapitalize="none"
+				autoCorrect={false}
+				value={enteredNumber}
+				onChangeText={numberInputHandler}
+			/>
+			<View style={styles.buttonsContainer}>
+				<View style={styles.buttonContainer}>
+					<PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+				</View>
+				<View style={styles.buttonContainer}>
+					<PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+				</View>
+			</View>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	inputContainer: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginTop: 100,
+		marginHorizontal: 24,
+		padding: 16,
+		backgroundColor: '#3b021f',
+		borderRadius: 8,
+		elevation: 4,
+		shadowColor: 'black',
+		shadowOffset: { width: 0, height: 2 },
+		shadowRadius: 6,
+		shadowOpacity: 0.25,
+	},
+	numberInput: {
+		height: 50,
+		width: 50,
+		fontSize: 32,
+		borderBottomColor: '#ddb52f',
+		borderBottomWidth: 2,
+		color: '#ddb52f',
+		marginVertical: 8,
+		fontWeight: 'bold',
+		textAlign: 'center',
+	},
+	buttonsContainer: {
+		flexDirection: 'row',
+	},
+	buttonContainer: {
+		flex: 1,
+	},
+});
